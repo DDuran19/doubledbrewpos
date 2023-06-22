54ba2846fe49dd5543c7c36a47704f6e7d3a9499
Denver James Duran <denverjames.duran19@outlook.com>
2023-06-22 14:05:55 +0800

    removed unnecessary addon

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
-import copy from 'vite-plugin-copy';
 
 export default defineConfig({
   plugins: [
     react(),
-    copy({
-      patterns: [
-        { from: 'src/data/addons.json', to: 'addons.json' },
-        { from: 'src/data/cartItems.json', to: 'cartItems.json' },
-        { from: 'src/data/milkteaFlavors.json', to: 'milkteaFlavors.json' },
-        { from: 'src/data/series.json', to: 'series.json' },
-        { from: 'src/data/sizes.json', to: 'sizes.json' },
-      ],
-    }),
   ],
 });
